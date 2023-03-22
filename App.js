b691fe591b50c16b290a468c292ce28f8c4bf897
Akshay Ramen <akramen10@gmail.com>
2023-03-22 17:27:00 -0500

    added react navigation for buttons at the top

--- App.js
+++ App.js
@@ -4,15 +4,34 @@ import {Provider} from "react-redux";
 import HomeScreen from './screens/HomeScreen';
 import { store } from './Store';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import 'react-native-gesture-handler'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator, NativeStackView } from '@react-navigation/native-stack';
+import MapScreen from './screens/MapScreen';
 
 
 
 export default function App() {
+  const Stack = createNativeStackNavigator();
+  
   return (
     <Provider store={store}>
+      <NavigationContainer>
       <SafeAreaProvider>
-      <HomeScreen />  
+        <Stack.Navigator>
+            <Stack.Screen
+              name='HomeScreen'
+              component={HomeScreen}
+              options={{headerShown: false, }}
+             />
+              <Stack.Screen
+              name='MapScreen'
+              component={MapScreen}
+              options={{headerShown: false, }}
+             />
+        </Stack.Navigator>
       </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
 }
