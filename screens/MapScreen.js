77b6bc182e2d82223c66268fea48d265ed7ebc9d
Akshay Ramen <akramen10@gmail.com>
2023-03-28 17:55:25 -0500

    RideOptionsCard + Final Changes

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React from 'react';
 import tw from "tailwind-react-native-classnames";
 import MapView from 'react-native-maps';
@@ -6,11 +6,17 @@ import MapViewComponent from '../components/Map';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import NavigationCard from '../components/NavigationCard';
 import RideOptionsCard from "../components/RideOptionsCard";
+import { Icon } from "react-native-elements/dist/icons/Icon"
+import { useNavigation } from '@react-navigation/native';
 
 const MapScreen = () => {
   const Stack = createNativeStackNavigator();
+  const navigation = useNavigation();
   return (
     <View>
+      <TouchableOpacity  onPress={() => navigation.navigate("HomeScreen") } style={tw`bg-gray-100 absolute top-16 left-8 z-50 p-3 rounded-full shadow-lg`}>
+        <Icon name="home" />
+      </TouchableOpacity>
       <View style={tw `h-1/2`}>
         <MapViewComponent/>
     </View>
